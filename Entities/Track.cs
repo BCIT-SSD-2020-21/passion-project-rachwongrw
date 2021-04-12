16788308da68f02a5d193b90d472b3dfee7fe926
Stefan Kuznetsov <skuznetsov@posteo.net>
2021-04-12 12:09:31 -0700

    Added the audio track entity

---
+++ Entities/Track.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace passion_project.Entities
+{
+
+    public class Section
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("section_number")]
+        public string SectionNumber { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+        [JsonPropertyName("playtime")]
+        public string Playtime { get; set; }
+        [JsonPropertyName("file_name")]
+        public string FileName { get; set; }
+    }
+
+    public class Track
+    {
+        [JsonPropertyName("sections")]
+        public List<Section> Sections;
+    }
+
+}
--- Network/Librivox_API_Wrapper.cs
+++ Network/Librivox_API_Wrapper.cs
@@ -12,16 +12,57 @@ namespace passion_project.Network
     public class Librivox_API_Wrapper
     {
         private static readonly HttpClient client = new HttpClient();
-        private static readonly string url = "https://librivox.org/api/feed/audiobooks?format=json";
+        private static readonly string baseurl = "https://librivox.org/api/feed/audiobooks?format=json";
 
         public static async Task<BookList> GetBookList()
         {
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var streamTask = client.GetStreamAsync(url);
+            var streamTask = client.GetStreamAsync(baseurl);
             var books = await JsonSerializer.DeserializeAsync<BookList>(await streamTask);
             return books;
+        }
+
+        public static async Task<Book> GetBook(string id)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var url = String.Format("{0}&id={1}", baseurl, id);
+            try
+            {
+                var streamTask = client.GetStreamAsync(url);
+                var book = await JsonSerializer.DeserializeAsync<BookList>(await streamTask);
+                return book.Books[0];
+
+            }
+            catch (Exception)
+            {
+                return await Task.FromResult<Book>(null);
+            }
+
+        }
+
+        // Returns null, needs further adjustment
+        public static async Task<Track> GetTrack(string id)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var trackurl = "https://librivox.org/api/feed/audiotracks/?format=json&id={0}";
+            var url = String.Format(trackurl, id);
+            try
+            {
+                var streamTask = client.GetStreamAsync(url);
+                var track = await JsonSerializer.DeserializeAsync<Track>(await streamTask);
+                return track;
+
+            }
+            catch (Exception)
+            {
+                return await Task.FromResult<Track>(null);
+            }
+
         }
 
     }
