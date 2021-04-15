9972f64128819ca0c9bf88c50f864ce3e362372e
Stefan Kuznetsov <skuznetsov@posteo.net>
2021-04-15 10:49:42 -0700

    Added a basic user view

---
+++ ViewModels/UserVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace passion_project.ViewModels
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
